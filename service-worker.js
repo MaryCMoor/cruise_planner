8c97df8127ace4a88b43724f27890136f19dab8d
Mary Moor <93663385+MaryCMoor@users.noreply.github.com>
2026-04-08 08:58:46 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'cruise-planner-v1.0';
+const urlsToCache = [
+  './',
+  './index.html',
+  './login.html',
+  './itinerary.html',
+  './schedule.html',
+  './packing.html',
+  './notes.html',
+  'https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-compat.js',
+  'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-compat.js'
+];
+
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Service Worker: Caching files');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker: Activating...');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Service Worker: Clearing old cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        console.log('Service Worker: Offline');
+      })
+  );
+});
